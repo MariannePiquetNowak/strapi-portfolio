4f463ff1281feabc5e27ea7875cf076deb95a104
MariannePiquetNowak <mariannexpiquet@gmail.com>
2023-04-09 11:35:03 +0200

    install dependencies of project and init styles scss

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import './assets/styles/App.scss';
+
+function App() {
+  return (
+    <div className="App">
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <h2>coucou</h2>
+    </div>
+  );
+}
+
+export default App;
