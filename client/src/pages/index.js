b0d4f1ebd11105b52459e839f2a036eb557d360f
Marianne Piquet-Nowak <mariannexpiquet@gmail.com>
2023-04-17 17:10:38 +0200

    #90_Delete-React-to-Next

---
+++ client/src/pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+        <Head>
+            <meta charSet="utf-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <meta name="description" content="Marianne Piquet-Nowak's personal website" />
+            <title>Marianne Piquet-Nowak | Full-Stack Developer</title>
+        </Head>
+    </>
+  )
+}
