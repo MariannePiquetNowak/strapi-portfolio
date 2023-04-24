81f105878698097d8094f1b417de45c1bdf0274d
Marianne Piquet-Nowak <mariannexpiquet@gmail.com>
2023-04-24 12:25:17 +0200

    some changes

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -1,11 +1,14 @@
 import '@/styles/globals.css'
 import Navbar from '@/components/Navbar'
+import Layout from '@/components/Layout';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Navbar />
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </>
   )
 }
