0275052024893dfcc5e51f29e28788891d5ee5cb
MariannePiquetNowak <mariannexpiquet@gmail.com>
2023-04-28 12:05:49 +0200

    #97_Animation-Onload-Page - create Loader component - add scss to animate

---
+++ client/src/components/Loader.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+
+const Loader = () => {
+  useEffect(() => {
+    const loader = document.querySelector(".loader");
+    const body = document.querySelector("body");
+    setTimeout(() => {
+      body.style.overflowY = "scroll";
+      loader.classList.add("hidden");
+    }, 1000);
+  }, []);
+
+  return (
+    <div className="loader fixed z-50 w-screen h-screen">
+      <span>Loading... </span>
+      <div class="bg-gray-800 rounded-full h-2.5 dark:bg-gray-700">
+        <div class="bg-gray-800 rounded-full h-2.5 dark:bg-gray-700">
+          <div class="h-2.5 rounded-full"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -1,10 +1,12 @@
 import "@/styles/globals.scss";
 import Navbar from "@/components/Navbar";
 import Layout from "@/components/Layout";
+import Loader from "@/components/Loader";
 
 export default function App({ Component, pageProps }) {
   return (
     <div className="md:flex flex-col md:flex-row relative">
+      <Loader />
       <Navbar />
       <Layout>
         <Component {...pageProps} />
