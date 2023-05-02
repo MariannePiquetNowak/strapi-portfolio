ea285064fc3232df18d4ec8b56768f9356f81980
MariannePiquetNowak <mariannexpiquet@gmail.com>
2023-05-02 10:56:47 +0200

    #98_Experience-Section - add favicon - add arrow circle icon - create Experience Section - modify the scss - create expNavbar but finally this is not used

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -4,7 +4,7 @@ import Footer from './Footer'
 const Layout = ({children}) => {
   return (
     <div className="md:flex justify-end md:w-full lg:mt-16">
-      <div className="md:w-10/12 lg:w-11/12 pt-16 md:py-[0]">
+      <div className="container md:w-10/12 lg:w-11/12 pt-16 md:py-[0]">
         {children}
         <Footer />
       </div>
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,48 +1,75 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import { useState, useEffect, useRef } from 'react'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import { useState, useEffect } from "react";
+import useWindowSize from "../hooks";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
-export default function Home({ about, skills, skillIcons }) {
-  // Add Loading state at the end of the Home page
-
+export default function Home({ about, skills, skillIcons, experiences }) {
   /*  
     To explain, the problem I'm having is when I get the description, breaks line are not render 
     To work around this, I could use the property dangerouslySetInnerHTML (<p> tags)
     But there are a hydration problem. It's the reason I use useState & useEffect hooks
     */
-  const [aboutDesc, setAboutDesc] = useState(null)
-  const [skillsDesc, setSkillsDesc] = useState(null)
+  const [aboutDesc, setAboutDesc] = useState(null);
+  const [skillsDesc, setSkillsDesc] = useState(null);
+  const [xp, setExperience] = useState(null);
+  const size = useWindowSize();
 
-  useEffect(() => setAboutDesc(about?.attributes.description))
-  useEffect(() => setSkillsDesc(skills?.attributes.description))
+  useEffect(() => setAboutDesc(about?.attributes.description));
+  useEffect(() => setSkillsDesc(skills?.attributes.description));
 
   /* Intersection observer */
   const options = {
-    rootMargin: '-40% 0px',
+    rootMargin: "-40% 0px",
     threshold: 0,
-  }
+  };
 
-  const t = { rootMargin: "0px", threshold: 0,}
+  const t = { rootMargin: "0px", threshold: 0 };
 
   useEffect(() => {
-    const blocks = document.querySelectorAll('.reveal')
+    const blocks = document.querySelectorAll(".reveal");
 
     const observer = new IntersectionObserver((entries) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
-          entry.target.classList.add('active')
-          observer.unobserve(entry.target)
+          entry.target.classList.add("active");
+          observer.unobserve(entry.target);
         }
-      })
-    }, options)
+      });
+    }, options);
 
     blocks.forEach((block) => {
-      observer.observe(block)
-    })
-  }, [])
+      observer.observe(block);
+    });
+  }, []);
+
+  useEffect(() => {
+    const firstBtn = document.querySelector(".btn");
+    firstBtn.classList.add("active");
+    experiences?.attributes.experiences.data?.map((xp) => {
+      xp?.attributes.name_agency === firstBtn.textContent
+        ? setExperience(xp)
+        : null;
+    });
+  });
+
+  useEffect(() => {
+    // Ajouter la classe active au bouton cliqué
+    const btns = document.querySelectorAll(".btn");
+    btns.forEach((btn) => {
+      btn.addEventListener("click", (e) => {
+        btns.forEach((btn) => btn.classList.remove("active"));
+        e.target.classList.add("active");
+        experiences?.attributes.experiences.data?.map((xp) => {
+          xp?.attributes.name_agency === e.target.textContent
+            ? setExperience(xp)
+            : null;
+        });
+      });
+    });
+  }, []);
 
   return (
     <>
@@ -57,9 +84,9 @@ export default function Home({ about, skills, skillIcons }) {
       </Head>
       <section
         id="About"
-        className="test p-6 md:m-10 md:p-8 xl:mx-0 flex flex-col items-center justify-center lg:flex-row-reverse lg:h-[800px]"
+        className="p-6 my-16 md:m-10 md:p-8 xl:mx-0 flex flex-col items-center justify-center lg:flex-row-reverse lg:h-[800px]"
       >
-        <div className="xl:px-[2em] flex flex-col items-center justify-between lg:flex-row-reverse lg:h-auto">
+        <div className="xl:px-[2em] flex flex-col items-center justify-between lg:flex-row-reverse lg:w-full lg:h-auto">
           <div className="image-section flex justify-center w-80 h-auto lg:w-3/6 xl:w-2/5 2xl:w-1/3 lg:h-auto lg:translate-y-[-8em] lg:translate-x-[-3em]">
             <Image
               src={`http://localhost:1337${about?.attributes.image.data.attributes.url}`}
@@ -69,7 +96,7 @@ export default function Home({ about, skills, skillIcons }) {
               alt="Marianne Piquet-Nowak Avatar"
             />
           </div>
-          <div className="info py-5 lg:w-4/5 xl:w-2/4 lg:auto lg:mr-6">
+          <div className="info py-5 lg:w-4/5 xl:w-2/4 lg:h-auto lg:mr-6">
             <span>Hi! My Name is</span>
             <h1>{about?.attributes.title}</h1>
             <h2>{about?.attributes.sub_title}</h2>
@@ -92,7 +119,16 @@ export default function Home({ about, skills, skillIcons }) {
             />
           </div>
           <div className="info py-5 lg:w-4/5 xl:w-2/4 lg:auto lg:mr-6 lg:absolute lg:z-20">
-            <h1>{skills?.attributes.title}</h1>
+            <div className="flex items-end">
+              <Image
+                src={`/images/arrow-circle-gradient.svg`}
+                className="image-section__skills w-9 h-9 md:w-14 md:h-14 lg:mb-2 mr-2 md:mr-3"
+                width={10}
+                height={10}
+                alt="Marianne Piquet-Nowak puzzle illustration"
+              />
+              <h1>{skills?.attributes.title}</h1>
+            </div>
             <h2>{skills?.attributes.sub_title}</h2>
             <p dangerouslySetInnerHTML={{ __html: skillsDesc }}></p>
             <div>
@@ -115,31 +151,96 @@ export default function Home({ about, skills, skillIcons }) {
           </div>
         </div>
       </section>
-      <section className="Experiences reveal">Experiences Section</section>
+      <section
+        id="Experience"
+        className="reveal p-6 md:p-8 xl:mx-0 flex flex-col items-center justify-center lg:flex-row-reverse lg:h-[800px]"
+      >
+        <div className="xl:px-[2em] flex flex-col items-center justify-end lg:flex-row-reverse lg:h-auto w-full">
+          <div className="image-section flex justify-center w-80 h-auto lg:w-3/6 xl:w-2/5 2xl:w-1/3 lg:h-auto lg:translate-y-[-8em] lg:translate-x-[-3em] lg:absolute lg:z-20 lg:end-1">
+            <Image
+              src={`http://localhost:1337${experiences?.attributes.image.data.attributes.url}`}
+              className="image-section__experience w-full h-auto"
+              width={250}
+              height={100}
+              alt="Marianne Piquet-Nowak Experience illustration"
+            />
+          </div>
+
+          <div className="info py-5 lg:w-4/5 xl:w-2/4 lg:auto lg:mr-6 lg:absolute lg:z-20 w-full">
+            <div className="flex items-end">
+              <Image
+                src={`/images/arrow-circle-gradient.svg`}
+                className="image-section__experiences w-9 h-9 md:w-14 md:h-14 lg:mb-2 mr-2 md:mr-3"
+                width={10}
+                height={10}
+                alt="Marianne Piquet-Nowak Experience illustration"
+              />
+              <h1>{experiences?.attributes?.title}</h1>
+            </div>
+            <div className="w-full my-5">
+              <div className="xp-buttons flex flex-row">
+                {experiences?.attributes.experiences.data?.map((xp) => {
+                  return (
+                    <button key={`btn-${xp?.id}`} className="btn p-2">
+                      {xp?.attributes.name_agency}
+                    </button>
+                  );
+                })}
+              </div>
+
+              <div key={`experience-${xp?.id}`} className="xp-info py-10 md:p-5">
+                <h3 className="text-white font-bold underline decoration-1 decoration-solid underline-offset-4">
+                  {xp?.attributes.title_agency.toUpperCase()}
+                </h3>
+                <p className="pb-2">{xp?.attributes.date}</p>
+                <ul className="my-2 ml-5">
+                  {xp?.attributes.tasks.data?.map((task) => (
+                    <li className="task text-xs">
+                      {task?.attributes.description}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </section>
       <section className="Projects">Projects Section</section>
       <section className="Contact">Contact Section</section>
     </>
-  )
+  );
 }
 
 // Use the method getStaticProps to get socials in API
 export const getStaticProps = async () => {
-  const aboutRes = await fetch('http://localhost:1337/api/about?populate=*')
-  const aboutData = await aboutRes.json()
-  const about = aboutData.data
+  const aboutRes = await fetch(
+    `${process.env.NEXT_PUBLIC_API_PORTFOLIO_URL}about?populate=*`
+  );
+  const aboutData = await aboutRes.json();
+  const about = aboutData.data;
 
-  const skillsRes = await fetch('http://localhost:1337/api/skill?populate=*&populate=image&populate=skill_icons.icon')
-  const skillsData = await skillsRes.json()
-  const skills = skillsData.data
+  const skillsRes = await fetch(
+    `${process.env.NEXT_PUBLIC_API_PORTFOLIO_URL}skill?populate=*&populate=image&populate=skill_icons.icon`
+  );
+  const skillsData = await skillsRes.json();
+  const skills = skillsData.data;
+
+  const experienceRes = await fetch(
+    `${process.env.NEXT_PUBLIC_API_PORTFOLIO_URL}work-section?populate=*&populate=image&populate=experiences.tasks`
+  );
+  const experienceData = await experienceRes.json();
+  const experiences = experienceData.data;
 
   return {
     props: {
       about: about,
       skills: skills,
       skillIcons: skills.attributes.skill_icons.data,
+      experiences: experiences,
     },
-  }
-}
+  };
+};
 
 // Get image in relation field => comment by andrew-braun
 // https://forum.strapi.io/t/no-image-in-the-api-response/13281/5
